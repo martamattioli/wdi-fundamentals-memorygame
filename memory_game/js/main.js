7a4a6490d8d4499a8ed33a312ea465602d8a0989
Marta Mattioli <MartaPZ@Martas-MacBook-Pro.local>
2017-05-27 11:32:44 +0200

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,4 +1,26 @@
-var cards = ["queen", "queen", "king", "king"];
+var cards = [
+	{
+		rank: "queen",
+		suit: "hearts",
+		cardImage: "images/queen-of-hearts.jpg",
+	},
+	{
+		rank: "queen",
+		suit: "diamonds",
+		cardImage: "images/queen-of-diamonds.jpg",
+	},
+	{
+		rank: "king",
+		suit: "hearts",
+		cardImage: "images/king-of-hearts.jpg",
+	},
+	{
+		rank: "king",
+		suit: "diamonds",
+		cardImage: "images/king-of-diamonds.jpg",
+	} 
+];
+
 var cardInPlay = [];
 
 var checkForMatch = function() {
@@ -10,9 +32,11 @@ var checkForMatch = function() {
 }
 
 var flipCard = function(cardId) {
-	console.log("User flipped " + cards[cardId]);
+	console.log("User flipped " + cards[cardId].rank);
+	console.log(cards[cardId].suit);
+	console.log(cards[cardId].cardImage);
 
-	cardInPlay.push(cards[cardId])
+	cardInPlay.push(cards[cardId].rank)
 
 	if (cardInPlay.length === 2) {
 		checkForMatch();
